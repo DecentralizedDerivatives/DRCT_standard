edc5de5787f0dfe190b9fc2d4b3674429c9afd8a
themandalore <fett.nicholas@yahoo.com>
2017-11-20 20:58:30 -0500

    Create Oracle.sol

---
+++ Oracle.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.4.17;
+
+
+
+contract Oracle {
+
+  /*Variables*/
+
+  //Owner of the oracle
+  address private owner;
+
+  //Mapping of documents stored in the oracle
+  mapping(uint => uint) public oracle_values;
+
+  /*Events*/
+
+  event DocumentStored(uint _key, uint _value);
+
+  /*Functions*/
+
+  modifier onlyOwner {
+    require(msg.sender == owner);
+    _;
+  }
+
+  //Constructor - Sets owner
+  function Oracle() public {
+    owner = msg.sender;
+  }
+
+  //Allows the owner of the Oracle to store a document in the oracle_values mapping. Documents
+  //represent underlying values at a specified date (key).
+  function StoreDocument(uint _key, uint _value) public onlyOwner() {
+    oracle_values[_key] = _value;
+    DocumentStored(_key, _value);
+  }
+
+}
