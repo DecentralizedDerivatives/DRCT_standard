0e3ba4886a9e87aa816e741867fbd122a314bda2
themandalore <fett.nicholas@yahoo.com>
2017-11-21 12:20:36 -0500

    Update Factory_Interface.sol

--- Factory_Interface.sol
+++ Factory_Interface.sol
@@ -2,4 +2,5 @@ pragma solidity ^0.4.17;
 
 interface Factory_Interface {
   function createToken(uint _supply, address _owner, bool long) public returns (address created, uint tokenratio);
+  function payToken(address _party, bool long) public;
 }
