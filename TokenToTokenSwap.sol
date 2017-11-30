d66fc5e8809f3bc2ed78eeb44d65ad36cc82c462
themandalore <fett.nicholas@yahoo.com>
2017-11-30 12:37:43 -0500

    Update TokenToTokenSwap.sol

--- TokenToTokenSwap.sol
+++ TokenToTokenSwap.sol
@@ -1,5 +1,12 @@
 pragma solidity ^0.4.17;
 
+import "./Oracle_Interface.sol";
+import "./DRCT_Interface.sol";
+import "./Factory_Interface.sol";
+import "./ERC20_Interface.sol";
+import "./SafeMath.sol";
+
+
 contract TokenToTokenSwap {
 
   using SafeMath for uint256;
@@ -22,11 +29,11 @@ contract TokenToTokenSwap {
   address creator;
 
   //Address of an operator who will ensure forcePay is called at the end of the swap period
-  address public operator;
+  address operator;
 
   //The Oracle address (check for list at www.github.com/DecentralizedDerivatives/Oracles)
-  address public oracle_address;
-  Oracle oracle;
+  address oracle_address;
+  Oracle_Interface oracle;
 
   //Address of the Factory that created this contract
   address public factory_address;
@@ -40,11 +47,11 @@ contract TokenToTokenSwap {
   SwapState current_state;
 
   //Start and end dates of the swaps - format is the same as block.timestamp
-  uint public start_date;
-  uint public end_date;
+  uint start_date;
+  uint end_date;
 
   //This is the amount that the change will be calculated on.  10% change in rate on 100 Ether notional is a 10 Ether change
-  uint public multiplier;
+  uint multiplier;
 
   /*TODO description*/
   uint public share_long;
@@ -57,20 +64,16 @@ contract TokenToTokenSwap {
   uint pay_to_short_b;
 
   //Address of created long and short DRCT tokens
-  address public long_token_address;
-  address public short_token_address;
-
-  //Dsitributed DRCT tokens for the long and short parties
-  DRCT_Interface long_token;
-  DRCT_Interface short_token;
+  address long_token_address;
+  address short_token_address;
 
   //Number of DRCT Tokens distributed to both parties
   uint public num_DRCT_longtokens;
-   uint public num_DRCT_shorttokens;
+  uint public num_DRCT_shorttokens;
 
   //Addresses of ERC20 tokens used to enter the swap
-  address public token_a_address;
-  address public token_b_address;
+  address token_a_address;
+  address  token_b_address;
 
   //Tokens A and B used for the notional
   ERC20_Interface token_a;
@@ -87,13 +90,13 @@ contract TokenToTokenSwap {
   address token_b_party;
 
   uint duration;
+  uint fee;
+  DRCT_Interface token;
 
   /*Events*/
 
   //Emitted when a Swap is created
   event SwapCreation(address _token_a, address _token_b, uint _start_date, uint _end_date, address _creating_party);
-  //Emitted when a second party enteres the Swap
-  event SwapEntered(address _token_a, address _token_b, uint _start_date, uint _end_date, address _entering_party);
   //Emitted when the swap has been paid out
   event PaidOut(address _long_token, address _short_token);
 
@@ -102,16 +105,6 @@ contract TokenToTokenSwap {
   //Will proceed only if the contract is in the expected state
   modifier onlyState(SwapState expected_state) {
     require(expected_state == current_state);
-    _;
-  }
-
-  //Will proceed only if the sender is one of the participating parties, or the operator
-  modifier onlyPartiesOrOperator() {
-    require(
-      msg.sender == short_party ||
-      msg.sender == long_party ||
-      msg.sender == operator
-    );
     _;
   }
 
@@ -125,22 +118,14 @@ contract TokenToTokenSwap {
   * @param "_creator": Address of the person who created the contract
   * @param "_factory": Address of the factory that created this contract
   */
-  function TokenToTokenSwap(address _o_address, address _operator, address _creator, address _factory, uint _duration,uint _start_date, uint _multiplier,address _token_a_address,address _token_b_address) public {
+  function TokenToTokenSwap (address _factory_address, address _creator) public {
     current_state = SwapState.created;
-    oracle_address = _o_address;
-    oracle = Oracle(_o_address);
-    factory_address = _factory;
-    factory = Factory_Interface(_factory);
-    creator = _creator;
-    operator = _operator;
-    duration = _duration;
-    multiplier = _multiplier;
-    token_a_address = _token_a_address;
-    token_b_address = _token_b_address;
-    start_date = _start_date;
-    end_date = start_date.add(duration.mul(86400));
+    creator =_creator;
+    factory_address = _factory_address;
+  }
 
-    require(token_a_address != token_b_address);
+  function showPrivateVars() public returns (address _long_token_address, address _short_token_address, address _oracle_adress, address _token_a_address, address _token_b_address, uint _multiplier, uint _duration,uint _start_date, uint _end_date){
+    return (long_token_address,short_token_address, oracle_address, token_a_address, token_b_address, multiplier, duration, start_date, end_date);
   }
 
   /*
@@ -165,10 +150,13 @@ contract TokenToTokenSwap {
     require(
       msg.sender == creator
     );
+    factory = Factory_Interface(factory_address);
+    setVars();
+    end_date = start_date.add(duration.mul(86400));
     token_a_amount = _amount_a;
     token_b_amount = _amount_b;
 
-    premium = msg.value;
+    premium = this.balance;
     token_a = ERC20_Interface(token_a_address);
     token_a_party = msg.sender;
     if (_sender_is_long)
@@ -176,6 +164,10 @@ contract TokenToTokenSwap {
     else
       short_party = msg.sender;
     current_state = SwapState.open;
+  }
+  
+  function setVars() internal{
+      (oracle_address,operator,duration,multiplier,token_a_address,token_b_address,start_date) = factory.getVariables();
   }
 
   /*
@@ -220,7 +212,7 @@ contract TokenToTokenSwap {
   *
   * @param: "_tokens": Amount of DRCT Tokens to be created
   */
-  function createTokens() public onlyState(SwapState.started) onlyPartiesOrOperator() {
+  function createTokens() public onlyState(SwapState.started){
 
     //Ensure the contract has been funded by tokens a and b
     require(
@@ -253,11 +245,9 @@ contract TokenToTokenSwap {
     uint tokenratio = 1;
     if (_creator == long_party) {
       (long_token_address,tokenratio) = factory.createToken(token_a_amount, _creator,true);
-      long_token = DRCT_Interface(long_token_address);
       num_DRCT_longtokens = token_a_amount.div(tokenratio);
     } else if (_creator == short_party) {
       (short_token_address,tokenratio) = factory.createToken(token_b_amount, _creator,false);
-      short_token = DRCT_Interface(short_token_address);
       num_DRCT_shorttokens = token_b_amount.div(tokenratio);
     }
   }
@@ -269,9 +259,9 @@ contract TokenToTokenSwap {
   */
   function Calculate() internal {
     //require(now >= end_date);
-
-    uint start_value = RetrieveData(start_date);
-    uint end_value = RetrieveData(end_date);
+    oracle = Oracle_Interface(oracle_address);
+    uint start_value = oracle.RetrieveData(start_date);
+    uint end_value = oracle.RetrieveData(end_date);
 
     uint ratio;
     if (start_value > 0 && end_value > 0)
@@ -346,26 +336,30 @@ contract TokenToTokenSwap {
     require(current_state == SwapState.ready);
 
     //Loop through the owners of long and short DRCT tokens and pay them
-    uint long_count = long_token.addressCount();
-    uint short_count = short_token.addressCount();
+    
+    token = DRCT_Interface(long_token_address);
+    uint count = token.addressCount();
     //Indexing begins at 1 for DRCT_Token balances
-    for(uint i = 1; i < long_count; i++) {
-      address long_owner = long_token.getHolderByIndex(i);
-      uint to_pay_long = long_token.getBalanceByIndex(i);
-      assert(i == long_token.getIndexByAddress(long_owner));
+    for(uint i = 1; i < count; i++) {
+      address long_owner = token.getHolderByIndex(i);
+      uint to_pay_long = token.getBalanceByIndex(i);
+      assert(i == token.getIndexByAddress(long_owner));
       paySwap(long_owner, to_pay_long, true);
     }
-    for(uint j = 1; j < short_count; j++) {
-      address short_owner = short_token.getHolderByIndex(j);
-      uint to_pay_short = short_token.getBalanceByIndex(j);
-      assert(j == short_token.getIndexByAddress(short_owner));
+
+    token = DRCT_Interface(short_token_address);
+    count = token.addressCount();
+    for(uint j = 1; j < count; j++) {
+      address short_owner = token.getHolderByIndex(j);
+      uint to_pay_short = token.getBalanceByIndex(j);
+      assert(j == token.getIndexByAddress(short_owner));
       paySwap(short_owner, to_pay_short, false);
     }
 
     token_a.transfer(operator, token_a.balanceOf(address(this)));
     token_b.transfer(operator, token_b.balanceOf(address(this)));
 
-    PaidOut(long_token, short_token);
+    PaidOut(long_token_address, short_token_address);
     current_state = SwapState.ended;
     return true;
   }
@@ -402,18 +396,7 @@ contract TokenToTokenSwap {
   * Once two parties enter the swap, the contract is null after cancelled.
   */
   function Exit() public {
-    require(
-      current_state != SwapState.ended &&
-      msg.sender == long_party ||
-      msg.sender == short_party
-    );
-
-    /*
-    * If the current state is open, then the other party has not entered and the creator of the swap can restart with new values
-    * Otherwise, if the two parties agree to an Exit and have not distributed their DRCT tokens, they are sent their respective tokens
-    * and the swap is cancelled
-    */
-    if (current_state == SwapState.open && msg.sender == token_a_party) {
+   if (current_state == SwapState.open && msg.sender == token_a_party) {
       token_a.transfer(token_a_party, token_a_amount);
       if (premium>0){
         msg.sender.transfer(premium);
@@ -429,23 +412,8 @@ contract TokenToTokenSwap {
         current_state = SwapState.ended;
         if (premium > 0) { creator.transfer(premium);}
       }
-    } else if (msg.sender == operator){
-        require (long_token.balanceOf(address(this)) == num_DRCT_longtokens &&
-        short_token.balanceOf(address(this)) == num_DRCT_shorttokens) ;
-        token_a.transfer(operator, token_a_amount);
-        token_b.transfer(operator, token_b_amount);
-        current_state = SwapState.ended;
-      }
+    }
   }
 
-  /*
-  * Retrieves data from the oracle based on a given date
-  *
-  * @param "_date": Date to retrieve values for, in block.timestamp format
-  */
-  function RetrieveData(uint _date) public constant returns (uint data) {
-    uint value = oracle.oracle_values(_date);
-    return value;
-  }
 
 }
