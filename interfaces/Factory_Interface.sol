ad477fa722aa86fea465689d2b454432c576d757
themandalore <fett.nicholas@yahoo.com>
2017-12-02 22:57:04 -0500

    Update Factory_Interface.sol

--- interfaces/Factory_Interface.sol
+++ interfaces/Factory_Interface.sol
@@ -4,5 +4,6 @@ pragma solidity ^0.4.17;
 interface Factory_Interface {
   function createToken(uint _supply, address _owner, bool long) public returns (address created, uint tokenratio);
   function payToken(address _party, bool long) public;
+   function deployContract() public payable returns (address created);
   function getVariables() public view returns (address oracle_addr, address factory_operator, uint swap_duration, uint swap_multiplier, address token_a_addr, address token_b_addr, uint swap_start_date);
 }
