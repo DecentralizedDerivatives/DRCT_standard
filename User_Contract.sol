d877ca1b91473830d594a808f0f4d3aa340798df
themandalore <fett.nicholas@yahoo.com>
2017-12-04 11:30:35 -0500

    Update User_Contract.sol

--- User_Contract.sol
+++ User_Contract.sol
@@ -14,36 +14,48 @@ contract UserContract{
   Factory_Interface factory;
 
   address public factory_address;
+  address owner;
+  
+  function UserContract(){
+      owner = msg.sender;
+  }
 
-  function Initiate(uint _amounta, uint _amountb, uint _premium, bool _isLong) payable public returns (address) {
-    require(msg.value == _amounta);
-    address swap_contract = factory.deployContract(msg.sender);
-    swap = TokenToTokenSwap_Interface(swap_contract);
-    swap.CreateSwap.value(_premium)(_amounta, _amountb, _isLong);
+  function Initiate(address _swapadd, uint _amounta, uint _amountb, uint _premium, bool _isLong) payable public returns (bool) {
+    require(msg.value == _amounta + _premium);
+    swap = TokenToTokenSwap_Interface(_swapadd);
+    swap.CreateSwap.value(_premium)(_amounta, _amountb, _isLong, msg.sender);
     address token_a_address;
     address token_b_address;
     (token_a_address,token_b_address) = factory.getBase();
     token = Wrapped_Ether(token_a_address);
     token.CreateToken.value(msg.value)();
-    token.transfer(swap_contract,msg.value);
-    return swap_contract;
+    bool success = token.transfer(_swapadd,msg.value);
+    return success;
   }
 
-  function Enter(uint _amounta, uint _amountb, bool _isLong, address _swapadd) payable public {
+  function Enter(uint _amounta, uint _amountb, bool _isLong, address _swapadd) payable public returns(bool){
     require(msg.value ==_amountb);
     swap = TokenToTokenSwap_Interface(_swapadd);
-    swap.EnterSwap(_amounta, _amountb, _isLong);
+    swap.EnterSwap(_amounta, _amountb, _isLong,msg.sender);
     address token_a_address;
     address token_b_address;
     (token_a_address,token_b_address) = factory.getBase();
     token = Wrapped_Ether(token_b_address);
     token.CreateToken.value(msg.value)();
-    token.transfer(_swapadd,msg.value);
+    bool success = token.transfer(_swapadd,msg.value);
     swap.createTokens();
+    return success;
   }
 
+
   function setFactory(address _factory_address) public {
+      require (msg.sender == owner);
     factory_address = _factory_address;
     factory = Factory_Interface(factory_address);
+  }
+
+  function setOwner(address _new_owner) public{ 
+    require (msg.sender == owner); 
+    owner = _new_owner; 
   }
 }
