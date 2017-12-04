a4062ee49d970cdfc70039487872476d0953dfed
themandalore <fett.nicholas@yahoo.com>
2017-12-04 11:34:08 -0500

    Update TokenToTokenSwap_Interface.sol

--- interfaces/TokenToTokenSwap_Interface.sol
+++ interfaces/TokenToTokenSwap_Interface.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.17;
 
 //Swap interface- descriptions can be found in TokenToTokenSwap.sol
 interface TokenToTokenSwap_Interface {
-  function CreateSwap(uint _amount_a, uint _amount_b, bool _sender_is_long) public payable;
-  function EnterSwap(uint _amount_a, uint _amount_b, bool _sender_is_long) public;
+  function CreateSwap(uint _amount_a, uint _amount_b, bool _sender_is_long, address _senderAdd) public payable;
+  function EnterSwap(uint _amount_a, uint _amount_b, bool _sender_is_long, address _senderAdd) public;
   function createTokens() public;
 }
