7c1a17650c53ec1ab91bfc1cecc096770baee557
themandalore <fett.nicholas@yahoo.com>
2017-12-04 11:20:19 -0500

    Update Deployer.sol

--- Deployer.sol
+++ Deployer.sol
@@ -5,15 +5,23 @@ import "./TokenToTokenSwap.sol";
 //Swap Deployer Contract
 contract Deployer {
   address owner;
+  address factory;
 
   function Deployer(address _factory) public {
-    owner = _factory;
+    factory = _factory;
+    owner = msg.sender;
   }
 
   //TODO - payable?
-  function newContract(address _party) public payable returns (address created) {
-    require(msg.sender == owner);
-    address new_contract = new TokenToTokenSwap(owner, _party);
+  function newContract(address _party, address user_contract) public returns (address created) {
+    require(msg.sender == factory);
+    address new_contract = new TokenToTokenSwap(factory, _party, user_contract);
     return new_contract;
+  }
+
+   function setVars(address _factory, address _owner) public {
+    require (msg.sender == owner);
+    factory = _factory;
+    owner = _owner;
   }
 }
