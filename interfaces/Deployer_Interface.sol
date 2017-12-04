3379aee69f1da63735f3bc5ddb914ce970d2e5b8
themandalore <fett.nicholas@yahoo.com>
2017-12-04 11:33:48 -0500

    Update Deployer_Interface.sol

--- interfaces/Deployer_Interface.sol
+++ interfaces/Deployer_Interface.sol
@@ -2,5 +2,5 @@ pragma solidity ^0.4.17;
 
 //Swap Deployer functions - descriptions can be found in Deployer.sol
 interface Deployer_Interface {
-  function newContract(address _party) public payable returns (address created);
+  function newContract(address _party, address user_contract) public payable returns (address created);
 }
