14a3f9db385c4ba0e5b30282716ddf4784efb43b
themandalore <fett.nicholas@yahoo.com>
2017-12-11 14:33:48 -0500

    Update User_Contract.sol

--- User_Contract.sol
+++ User_Contract.sol
@@ -6,8 +6,7 @@ import "./interfaces/Factory.sol";
 import "./WrappedEther.sol";
 import "./libraries/SafeMath.sol";
 
-
-
+//The User Contract enables the entering of a deployed swap along with the wrapping of Ether.  This contract was specifically made for drct.decentralizedderivatives.org to simplify user metamask calls
 contract UserContract{
   TokenToTokenSwap_Interface swap;
   Wrapped_Ether token;
@@ -19,6 +18,12 @@ contract UserContract{
   function UserContract() public {
       owner = msg.sender;
   }
+
+  //The _swapAdd is the address of the deployed contract created from the Factory contract.  
+  //_amounta and _amountb are the amounts of token_a and token_b (the base tokens) in the swap.  For wrapped Ether, this is wei.  
+  //_premium is a base payment to the other party for taking the other side of the swap
+  // _isLong refers to whether the sender is long or short the reference rate
+  //Value must be sent with Initiate and Enter equivalent to the _amounta(in wei) and the premium, and _amountb respectively
 
   function Initiate(address _swapadd, uint _amounta, uint _amountb, uint _premium, bool _isLong) payable public returns (bool) {
     require(msg.value == _amounta + _premium);
