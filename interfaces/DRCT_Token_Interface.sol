7c959d69c5a0a428a42f08266fe3ed2c43ffadf3
themandalore <fett.nicholas@yahoo.com>
2017-12-11 14:32:02 -0500

    Update DRCT_Token_Interface.sol

--- interfaces/DRCT_Token_Interface.sol
+++ interfaces/DRCT_Token_Interface.sol
@@ -4,14 +4,9 @@ pragma solidity ^0.4.17;
 interface DRCT_Token_Interface {
   function addressCount(address _swap) public constant returns (uint count);
   function getHolderByIndex(uint _ind, address _swap) public constant returns (address holder);
-  /*function getDeepHolderByIndex(uint _ind, address _swap) public constant returns (address holder);*/
-
   function getBalanceByIndex(uint _ind, address _swap) public constant returns (uint bal);
-
-  /*function getBalanceByIndex(uint _ind) public constant returns (uint bal);*/
   function getIndexByAddress(address _owner, address _swap) public constant returns (uint index);
   function createToken(uint _supply, address _owner, address _swap) public;
   function pay(address _party, address _swap) public;
   function partyCount(address _swap) public constant returns(uint count);
 }
-
