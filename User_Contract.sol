96d16d323995cdefec4276d6971c3c4515559643
Bradley Stachurski <bradley.stachurski@gmail.com>
2017-12-19 08:14:59 -0700

    Changed import statements

--- User_Contract.sol
+++ User_Contract.sol
@@ -2,8 +2,8 @@ pragma solidity ^0.4.17;
 
 
 import "./interfaces/TokenToTokenSwap_Interface.sol";
-import "./interfaces/Factory.sol";
-import "./WrappedEther.sol";
+import "./interfaces/Factory_Interface.sol";
+import "./Wrapped_Ether.sol";
 import "./libraries/SafeMath.sol";
 
 //The User Contract enables the entering of a deployed swap along with the wrapping of Ether.  This contract was specifically made for drct.decentralizedderivatives.org to simplify user metamask calls
@@ -19,8 +19,8 @@ contract UserContract{
       owner = msg.sender;
   }
 
-  //The _swapAdd is the address of the deployed contract created from the Factory contract.  
-  //_amounta and _amountb are the amounts of token_a and token_b (the base tokens) in the swap.  For wrapped Ether, this is wei.  
+  //The _swapAdd is the address of the deployed contract created from the Factory contract.
+  //_amounta and _amountb are the amounts of token_a and token_b (the base tokens) in the swap.  For wrapped Ether, this is wei.
   //_premium is a base payment to the other party for taking the other side of the swap
   // _isLong refers to whether the sender is long or short the reference rate
   //Value must be sent with Initiate and Enter equivalent to the _amounta(in wei) and the premium, and _amountb respectively
