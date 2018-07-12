15f16e008901d74d70a71ae858bb2dd8f94dba1c
Brenda <l.loya.b@gmail.com>
2018-07-12 12:36:30 -0400

    scripts updates

--- scripts/Admin_3_1_update_oracle.js
+++ scripts/Admin_3_1_update_oracle.js
@@ -1,6 +1,6 @@
 /**
-*Deploy new oracle with updated API information and 
-*update the factory using deprecated oracle.
+*
+*update the factory using deprecated oracle with an existing oracle
 */
 var MasterDeployer = artifacts.require("MasterDeployer");
 var Oracle = artifacts.require("Oracle");
@@ -15,21 +15,22 @@ var Factory = artifacts.require("Factory");
 *
 *Update oracle APIs(two are needed, the second one is used as backup)
 */
-
-var _oracle_api = "json(https://api.gdax.com/products/ETH-USD/ticker).price";
-var _oracle_api2 = "json(https://api.binance.com/api/v3/ticker/price?symbol=ETHUSDT).price";
+//ETH oracle
+var _oracle = "0xb9348a358ad2e632084f26206390332f8bb34de8";
 var _factory = "0xd47823a9769bec0bc31d3ef8b076d820865f39d0";
 
-//var _oracle_api = "json(https://api.gdax.com/products/BTC-USD/ticker).price";
-//var _oracle_api2 = "json(https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT).price";
+
+
+//BTC oracle
+//var _oracle = "0xb666c8682cc9f279f1133476eaf0365778dc3c71";
 //var _factory = "0xb4dab81e95719ea69f08616c221e42489e84da3a";
 
 module.exports =async function(callback) {
     let factory;
-    let oracle;
+    //let oracle;
     factory = await Factory.at(_factory);
-    oracle = await Oracle.new(_oracle_api,_oracle_api2);
-    await factory.setOracleAddress(oracle.address);
+    //oracle = await Oracle.at(_oracle);
+    await factory.setOracleAddress(_oracle);
     console.log('Factory : ',factory.address);
-    console.log('Oracle: ',oracle.address);
+    console.log('Oracle: ',_oracle);
 }
