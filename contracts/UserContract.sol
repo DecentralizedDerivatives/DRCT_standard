8e5069b213bb1d550c8d90199c4f976f8cdb077b
themandalore <fett.nicholas@yahoo.com>
2018-09-20 21:01:25 -0400

    compiling

--- contracts/UserContract.sol
+++ contracts/UserContract.sol
@@ -19,13 +19,13 @@ contract UserContract{
 
     /*Variables*/
     TokenToTokenSwap_Interface internal swap;
-    Wrapped_Ether internal baseToken;
+    Wrapped_Ether internal weth;
     Factory internal factory; 
     address public factory_address;
     address public wrapped_address;
     address internal owner;
+    address public dai_address;
     OtcInterface oasisDex;
-    WETHInterface weth;
     ERC20_Interface dai;
 
 
@@ -44,9 +44,9 @@ contract UserContract{
         require(msg.value == _amount.mul(2));
         swap = TokenToTokenSwap_Interface(_swapadd);
         address token_address = factory.token();
-        baseToken = Wrapped_Ether(token_address);
-        baseToken.createToken.value(_amount.mul(2))();
-        baseToken.transfer(_swapadd,_amount.mul(2));
+        weth = Wrapped_Ether(token_address);
+        weth.deposit.value(_amount.mul(2))();
+        weth.transfer(_swapadd,_amount.mul(2));
         swap.createSwap(_amount, msg.sender);
     }
 
@@ -65,46 +65,49 @@ contract UserContract{
         wrapped_address = _wrapped;
     }
 
+    function setDaiAddress(address _dai) public {
+        require (msg.sender == owner);
+        dai_address = _dai;
+        dai = ERC20_Interface(_dai);
+    }
+
 
 
     /**
     *@dev Value must be sent with Initiate and enter the _amount(in wei) 
     *@param _swapadd is the address of the deployed contract created from the Factory contract
-    *@param _amount is the amount of the base tokens(short or long) in the
-    *swap. For wrapped Ether, this is wei.
+    *@param _amount is the amount of the base tokens(short or long) in the swap. For wrapped Ether, this is wei.
+    *@param  _minBuyAmt minimum amount of ETH you want returned
     */
-    function InitiateWithDai(address _swapadd, uint _amount) payable public{
+    function InitiateWithDai(address _swapadd, uint _amount,uint _minBuyAmt) payable public{
         require(msg.value == _amount.mul(2));
         swap = TokenToTokenSwap_Interface(_swapadd);
         // Give max allowance to oasisdex to spend weth
-        baseToken = Wrapped_Ether(token_address);
-        dai = ERC20(_dai);
+        require(dai_address == factory.token());
+        weth = Wrapped_Ether(wrapped_address);
         weth.deposit.value(msg.value)();
           // Give max allowance to oasisdex to spend weth
         if (weth.allowance(this, oasisDex) < msg.value) {
           weth.approve(oasisDex, uint(-1));
         }
         // Sell the WETH for a minBuyAmt of DAI
-        uint daiAmt = oasisDex.sellAllAmount(weth, msg.value, dai, _minBuyAmt);
+        uint daiAmt = oasisDex.sellAllAmount(wrapped_address, msg.value, dai_address, _minBuyAmt);
         dai.transfer(_swapadd,daiAmt);
         swap.createSwap(daiAmt/2, msg.sender);
     }
 
         /**
-    *@dev Value must be sent with Initiate and enter the _amount(in wei) 
-    *@param _swapadd is the address of the deployed contract created from the Factory contract
-    *@param _amount is the amount of the base tokens(short or long) in the
-    *swap. For wrapped Ether, this is wei.
+    *@dev Lets you sell your DAI on OasisDex
+    *@param  _minBuyAmt minimum amount of ETH you want returned
     */
-    function CashOutDai() public returns(uint _eth){
-        dai = ERC20(_dai);
-        if (weth.allowance(this, oasisDex) < _amount) {
+    function CashOutDai(uint _minBuyAmt) public returns(uint _eth){
+        uint _dai = dai.balanceOf(msg.sender);
+        if (weth.allowance(this, oasisDex) < _dai) {
           weth.approve(oasisDex, uint(-1));
         }
-        // Sell the WETH for a minBuyAmt of DAI
-        uint daiAmt = oasisDex.buyAllAmount(weth, msg.value, dai, _minBuyAmt);
-        weth.withdraw(daiAmt);
-        return daiAmt;
-
+        // Sell the DAI for a minimum amount of WETH
+        uint Amt = oasisDex.sellAllAmount(dai_address,_dai, wrapped_address, _minBuyAmt);
+        weth.withdraw(Amt);
+        return Amt;
     }
 }
--- contracts/Wrapped_Ether.sol
+++ contracts/Wrapped_Ether.sol
@@ -1,121 +1,80 @@
 pragma solidity ^0.4.24;
 import "./libraries/SafeMath.sol";
 
+// Copyright (C) 2015, 2016, 2017 Dapphub
 
-/**
-*This is the basic wrapped Ether contract. 
-*All money deposited is transformed into ERC20 tokens at the rate of 1 wei = 1 token
-*/
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+pragma solidity ^0.4.18;
+
 contract Wrapped_Ether {
+    string public name     = "Wrapped Ether";
+    string public symbol   = "WETH";
+    uint8  public decimals = 18;
 
-    using SafeMath for uint256;
+    event  Approval(address indexed src, address indexed guy, uint wad);
+    event  Transfer(address indexed src, address indexed dst, uint wad);
+    event  Deposit(address indexed dst, uint wad);
+    event  Withdrawal(address indexed src, uint wad);
 
-    /*Variables*/
+    mapping (address => uint)                       public  balanceOf;
+    mapping (address => mapping (address => uint))  public  allowance;
 
-    //ERC20 fields
-    string public name = "Wrapped Ether";
-    uint public total_supply;
-    mapping(address => uint) internal balances;
-    mapping(address => mapping (address => uint)) internal allowed;
-
-    /*Events*/
-    event Transfer(address indexed _from, address indexed _to, uint _value);
-    event Approval(address indexed _owner, address indexed _spender, uint _value);
-    event StateChanged(bool _success, string _message);
-
-    /*Functions*/
-    /**
-    *@dev This function creates tokens equal in value to the amount sent to the contract
-    */
-    function createToken() public payable {
-        require(msg.value > 0);
-        balances[msg.sender] = balances[msg.sender].add(msg.value);
-        total_supply = total_supply.add(msg.value);
+    function() public payable {
+        deposit();
+    }
+    function deposit() public payable {
+        balanceOf[msg.sender] += msg.value;
+        emit Deposit(msg.sender, msg.value);
+    }
+    function withdraw(uint wad) public {
+        require(balanceOf[msg.sender] >= wad);
+        balanceOf[msg.sender] -= wad;
+        msg.sender.transfer(wad);
+        emit Withdrawal(msg.sender, wad);
     }
 
-    /**
-    *@dev This function 'unwraps' an _amount of Ether in the sender's balance by transferring 
-    *Ether to them
-    *@param _value The amount of the token to unwrap
-    */
-    function withdraw(uint _value) public {
-        balances[msg.sender] = balances[msg.sender].sub(_value);
-        total_supply = total_supply.sub(_value);
-        msg.sender.transfer(_value);
+    function totalSupply() public view returns (uint) {
+        return address(this).balance;
     }
 
-    /**
-    *@param _owner is the owner address used to look up the balance
-    *@return Returns the balance associated with the passed in _owner
-    */
-    function balanceOf(address _owner) public constant returns (uint bal) { 
-        return balances[_owner]; 
-    }
-
-    /**
-    *@dev Allows for a transfer of tokens to _to
-    *@param _to The address to send tokens to
-    *@param _amount The amount of tokens to send
-    */
-    function transfer(address _to, uint _amount) public returns (bool) {
-        if (balances[msg.sender] >= _amount
-        && _amount > 0
-        && balances[_to] + _amount > balances[_to]) {
-            balances[msg.sender] = balances[msg.sender] - _amount;
-            balances[_to] = balances[_to] + _amount;
-            emit Transfer(msg.sender, _to, _amount);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    /**
-    *@dev Allows an address with sufficient spending allowance to send tokens on the behalf of _from
-    *@param _from The address to send tokens from
-    *@param _to The address to send tokens to
-    *@param _amount The amount of tokens to send
-    */
-    function transferFrom(address _from, address _to, uint _amount) public returns (bool) {
-        if (balances[_from] >= _amount
-        && allowed[_from][msg.sender] >= _amount
-        && _amount > 0
-        && balances[_to] + _amount > balances[_to]) {
-            balances[_from] = balances[_from] - _amount;
-            allowed[_from][msg.sender] = allowed[_from][msg.sender] - _amount;
-            balances[_to] = balances[_to] + _amount;
-            emit Transfer(_from, _to, _amount);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    /**
-    *@dev This function approves a _spender an _amount of tokens to use
-    *@param _spender address
-    *@param _amount amount the spender is being approved for
-    *@return true if spender appproved successfully
-    */
-    function approve(address _spender, uint _amount) public returns (bool) {
-        allowed[msg.sender][_spender] = _amount;
-        emit Approval(msg.sender, _spender, _amount);
+    function approve(address guy, uint wad) public returns (bool) {
+        allowance[msg.sender][guy] = wad;
+        emit Approval(msg.sender, guy, wad);
         return true;
     }
 
-    /**
-    *@param _owner address
-    *@param _spender address
-    *@return Returns the remaining allowance of tokens granted to the _spender from the _owner
-    */
-    function allowance(address _owner, address _spender) public view returns (uint) {
-       return allowed[_owner][_spender]; }
+    function transfer(address dst, uint wad) public returns (bool) {
+        return transferFrom(msg.sender, dst, wad);
+    }
 
-    /**
-    *@dev Getter for the total_supply of wrapped ether
-    *@return total supply
-    */
-    function totalSupply() public constant returns (uint) {
-       return total_supply;
+    function transferFrom(address src, address dst, uint wad)
+        public
+        returns (bool)
+    {
+        require(balanceOf[src] >= wad);
+
+        if (src != msg.sender && allowance[src][msg.sender] != uint(-1)) {
+            require(allowance[src][msg.sender] >= wad);
+            allowance[src][msg.sender] -= wad;
+        }
+
+        balanceOf[src] -= wad;
+        balanceOf[dst] += wad;
+
+        emit Transfer(src, dst, wad);
+
+        return true;
     }
 }
